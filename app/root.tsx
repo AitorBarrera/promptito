055456fa0bfd4843347f31e2aff80f5425515c4a
aitor <alumno.138627@ies-azarquiel.es>
2025-05-27 14:18:45 +0200

    Revert "prueba de vercel speed insight y actualizacion de node"
    This reverts commit 4654fabd04c281ef956e7e6a72b4a74c95d04ff2.

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,6 @@ import { dark } from "@clerk/themes";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { colorThemeMaterial } from "./styles/colorThemeMaterial";
 import { UserProvider } from "./contexts/UserProvider";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -61,7 +60,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <SpeedInsights />
         <ScrollRestoration />
         <Scripts />
       </body>
